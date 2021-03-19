ba0995d3124a743f7621117c9d6fa0f5e38d30f0
henrychoi7 <wschd770@gmail.com>
2021-03-19 05:29:38 +0000

    Deploying to gh-pages from  @ c4a6bde129d41fd4388d08f57991457c5343135d 🚀

---
+++ _nuxt/static/1616131776/privacy/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1616131776",layout:"default",error:a,state:{plans:{},locales:{locales:[b,"ko"],locale:b}},serverRendered:true,routePath:"\u002Fprivacy",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"en"));
