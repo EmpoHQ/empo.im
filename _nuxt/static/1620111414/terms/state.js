6dc19071be9bc3cc3d19adf7afe748959cb1d6c5
henrychoi7 <wschd770@gmail.com>
2021-05-04 06:56:56 +0000

    Deploying to gh-pages from  @ 3e086b74c54545b2085ebd70c4a2a987c7555224 🚀

---
+++ _nuxt/static/1620111414/terms/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620111414",layout:"default",error:a,state:{plans:{},locales:{locales:[b,"ko"],locale:b}},serverRendered:true,routePath:"\u002Fterms",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"en"));
