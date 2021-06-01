23290bf0c10ed48b0c2405d47b2528ff303ea1f1
henrychoi7 <wschd770@gmail.com>
2021-06-01 01:36:58 +0000

    Deploying to gh-pages from @ EmpoHQ/empo.im@dfed77678e68e83d37f61b68aebf5a831e36c971 🚀

---
+++ _nuxt/static/1622511415/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622511415",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{plans:{},locales:{locales:[b,"ko"],locale:b}},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Fabout","en"));
